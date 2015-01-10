9caa2679465ab2298e85dede20dc4f52bda95d66
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-01-10 13:55:15 -0500

    rss aggregator paired with readability API parser

---
+++ aggregate.py
@@ -0,0 +1,74 @@
+import csv
+import logging
+import argparse
+import datetime
+
+import urllib2
+import xml.etree.ElementTree as etree
+
+from parser import parse_content
+
+
+def aggregate(mode='rss'):
+    fetched_content = []
+    if mode == 'rss':
+        for category, urls in generate_rss_links():
+            for url in fetch_links(urls):
+                print "CHECK URL:\t"
+                parsed_content = parse_content(url)
+                if parsed_content['title'] != 'Log In' \
+                        and parsed_content['lead_image_url']:
+                    fetched_content.append(parsed_content)
+                    print parsed_content['title']
+
+    elif mode == 'twitter':
+        print 'twitter mode'
+    else:
+        print 'error'
+
+
+def generate_rss_links():
+    with open('sources/rss.csv', 'rb') as f:
+        csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+        for line in csv_reader:
+            category, link = line
+            yield category.strip(), link.strip()
+
+
+def fetch_links(urls):
+    try:
+        content = ''.join(urllib2.urlopen(urls))
+        root = etree.fromstring(content)
+
+        for i in parse_links(root):
+            yield i
+
+    except Exception as e:
+        print e
+
+
+def parse_links(node):
+    itr = node.iter()
+    try:
+        for i in itr:
+            if i.tag in ('item',):
+                for j in i.getchildren():
+                    if j.tag in ('link',):
+                        yield j.text
+    except StopIteration:
+        pass
+
+
+def logging_config():
+    log_name = 'logs/aggregate_' + str(datetime.datetime.now()) + '.log'
+    logging.basicConfig(filename=log_name, level=logging.INFO)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fetch content given \
+      twitter username, domain combinations or rss list')
+    parser.add_argument('-m', '--mode', help='rss for RSS feed, \
+      twitter for Twitter feed')
+    args = vars(parser.parse_args())
+
+    aggregate(mode=args['mode'])
---
+++ parser.py
@@ -0,0 +1,31 @@
+import urllib2
+import json
+from credentials import readability_api_token
+
+
+def parse_content(url, version='v1'):
+    '''
+    Parse URL to extract relevent content information
+
+    Reference:
+        https://www.readability.com/developers/api/parser
+
+    Returns:
+        {
+            'domain',
+            'title',
+            'date_published',
+            'author',
+            'excerpt',
+            'content',
+            'word_count',
+            'lead_image_url',
+        }
+    '''
+    url = url.replace("#", "%23")
+    endpoint = 'http://readability.com/api/content/' \
+        + version + '/parser?url=' \
+        + url + '&token=' + readability_api_token
+    req = urllib2.urlopen(endpoint)
+    res = json.loads(req.read())
+    return res
